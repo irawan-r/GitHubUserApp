5fb05d0cf31550b816da6778557fbf613626d0c9
irawan-r <irawanr567@gmail.com>
2022-01-16 12:32:51 +0700

    GitHubUser App

--- app/src/main/java/com/roy/githubuser/dataclass/UserResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.roy.githubuser.dataclass
-
-data class UserResponse(
-
-    @field:SerializedName("items")
-    val items: ArrayList<User>
-)
-
-annotation class SerializedName(val value: String)
