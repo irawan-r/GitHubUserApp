ab7444f0504bf302db6e815f734cfe2560c3fc91
irawan-r <irawanr567@gmail.com>
2022-01-22 12:46:34 +0700

    Fix splashscreen

--- app/src/main/java/com/roy/githubuser/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/roy/githubuser/ui/SplashScreenActivity.kt
@@ -1,23 +1,25 @@
 package com.roy.githubuser.ui
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.roy.githubuser.R
-import org.jetbrains.anko.startActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     companion object {
-        const val DELAY_TIME = 2000
+        const val DELAY_TIME = 3000
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
         Handler(mainLooper).postDelayed({
-            startActivity<MainActivity>()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
             finish()
         }, DELAY_TIME.toLong())
     }
