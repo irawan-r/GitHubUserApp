9bb958848bb70843e33007958ccf0a1838cba4cd
irawan-r <irawanr567@gmail.com>
2022-05-26 18:48:12 +0700

    Adding github token

--- app/src/main/java/com/roy/githubuser/api/ApiService.kt
+++ app/src/main/java/com/roy/githubuser/api/ApiService.kt
@@ -1,5 +1,6 @@
 package com.roy.githubuser.api
 
+import com.roy.githubuser.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -8,25 +9,27 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token ${com.roy.githubuser.BuildConfig.GITHUB_TOKEN}")
+    @Headers("Authorization: token $GITHUB_TOKEN")
     fun getSearchUser(@Query("q") query: String): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token ${com.roy.githubuser.BuildConfig.GITHUB_TOKEN}")
+    @Headers("Authorization: token $GITHUB_TOKEN")
     fun getUserDetail(
         @Path("username") username: String?
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token ${com.roy.githubuser.BuildConfig.GITHUB_TOKEN}")
+    @Headers("Authorization: token $GITHUB_TOKEN")
     fun getFollowers(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token ${com.roy.githubuser.BuildConfig.GITHUB_TOKEN}")
+    @Headers("Authorization: token $GITHUB_TOKEN")
     fun getFollowing(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 }
+
+const val GITHUB_TOKEN: String = BuildConfig.GITHUB_TOKEN
 
